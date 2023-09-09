ff8074ff2b54aeb0f097fbe23893f7e5f2c33cd4
darthspectrum <muradianator@gmail.com>
2023-09-09 21:03:27 +0400

    chore: watching the tests and running them

--- watcher.sh
+++ watcher.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-while inotifywait -e modify ./src; do
+while inotifywait -e modify ./src ./tests; do
   ELM_FILES=$(find ./src -name '*.elm')
+
   echo "Watching $ELM_FILES"
-  elm make $ELM_FILES
+  elm make $ELM_FILES && elm-test
 done
