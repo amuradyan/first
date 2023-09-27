2d57f4892d9ff367ccd7035d1cba0305051693d2
darthspectrum <muradianator@gmail.com>
2023-09-27 21:15:20 +0400

    chore: formatting

--- src/Xaxahraparak.elm
+++ src/Xaxahraparak.elm
@@ -1,42 +1,55 @@
 module Xaxahraparak exposing (..)
 
 -- Rules
-
 -- - Ոնց կարա գառաժը երեխուն խանգարի
 -- - Երեխուն կարա ուրիշ երեխա խանգարի
 -- - Երեխուն կարա ուրիշ ավելի մեծ երեխա խանգարի
 
-type alias OneDXaxahraparak = List OneDYachejka
-type alias Coordinate = Int
-type Occupant = Garaj | Erexa
+
+type alias OneDXaxahraparak =
+    List OneDYachejka
+
+
+type alias Coordinate =
+    Int
+
+
+type Occupant
+    = Garaj
+    | Erexa
+
 
 type alias OneDYachejka =
-  {
-    occupants: List Occupant
-    , x: Coordinate
-  }
+    { occupants : List Occupant
+    , x : Coordinate
+    }
 
 
-spawn: Int -> OneDXaxahraparak
+spawn : Int -> OneDXaxahraparak
 spawn size =
-  List.range 0 (size - 1)
-  |> List.map (\coordinate -> {occupants = [], x = coordinate})
+    List.range 0 (size - 1)
+        |> List.map (\coordinate -> { occupants = [], x = coordinate })
 
 
-populate: OneDXaxahraparak -> Coordinate -> Occupant -> OneDXaxahraparak
+populate : OneDXaxahraparak -> Coordinate -> Occupant -> OneDXaxahraparak
 populate xaxahraparak coordinate occupant =
-  case (xaxahraparak, coordinate) of
-    ([], _) -> []
-    ((x::xs), 0) -> {occupants = occupant :: x.occupants, x = x.x} :: xs
-    ((x::xs), c) -> x :: (populate xs (coordinate - 1) occupant)
+    case ( xaxahraparak, coordinate ) of
+        ( [], _ ) ->
+            []
+
+        ( x :: xs, 0 ) ->
+            { occupants = occupant :: x.occupants, x = x.x } :: xs
+
+        ( x :: xs, _ ) ->
+            x :: populate xs (coordinate - 1) occupant
 
 
+
+-- @->-
 -- 0    +
 -- 000  +
 -- 00e  +
 -- |-0g0
 -- |- 00e -> 0e0
-
-
 -- type Napravlennost = Horizontal| Vertical
 -- type alias Garaj = {position: (Int,Int), napravlennost: Napravlennost, length: Int}
--- tests/XaxahraparakSpec.elm
+++ tests/XaxahraparakSpec.elm
@@ -2,48 +2,61 @@ module XaxahraparakSpec exposing (..)
 
 import Expect exposing (Expectation)
 import Test exposing (..)
-import Xaxahraparak exposing (spawn, populate, Occupant(..))
+import Xaxahraparak exposing (Occupant(..), populate, spawn)
+
 
 suite : Test
 suite =
-  let
-    pending: () -> Expectation
-    pending = \() -> Expect.fail "pending"
-  in
-    describe "Xaxahraparak generation" [
-      test "creating prisitne xaxahraparaks" <|
-        \() ->
-          let
-            gaugeXaxahraparak = [{occupants = [], x = 0}, {occupants = [], x = 1}, {occupants = [], x = 2}]
-            realXaxahraparak = spawn 3
-          in
-            Expect.equal realXaxahraparak gaugeXaxahraparak
+    let
+        pending : () -> Expectation
+        pending =
+            \() -> Expect.fail "pending"
+    in
+    describe "Xaxahraparak generation"
+        [ test "creating prisitne xaxahraparaks" <|
+            \() ->
+                let
+                    gaugeXaxahraparak =
+                        [ { occupants = [], x = 0 }, { occupants = [], x = 1 }, { occupants = [], x = 2 } ]
 
-      , test "ete xaxahraparaki chaperic dus a qcum, hin vichakn a mnum" <|
-        \() ->
-          let
-            initialXaxahraparak = [{occupants = [], x = 0}, {occupants = [], x = 1}]
-            populatedXaxahraparak = populate initialXaxahraparak 3 Erexa
-          in
-            Expect.equal initialXaxahraparak populatedXaxahraparak
+                    realXaxahraparak =
+                        spawn 3
+                in
+                Expect.equal realXaxahraparak gaugeXaxahraparak
+        , test "ete xaxahraparaki chaperic dus a qcum, hin vichakn a mnum" <|
+            \() ->
+                let
+                    initialXaxahraparak =
+                        [ { occupants = [], x = 0 }, { occupants = [], x = 1 } ]
 
-      , test "ete nes a qcum, de hajtnvum a" <|
-        \() ->
-          let
-            initialXaxahraparak = [{occupants = [], x = 0}, {occupants = [], x = 1}]
-            populatedXaxahraparak = populate initialXaxahraparak 1 Erexa
-            gaugeXaxahraparak = [{occupants = [], x = 0}, {occupants = [Erexa], x = 1}]
-          in
-            Expect.equal populatedXaxahraparak gaugeXaxahraparak
+                    populatedXaxahraparak =
+                        populate initialXaxahraparak 3 Erexa
+                in
+                Expect.equal initialXaxahraparak populatedXaxahraparak
+        , test "ete nes a qcum, de hajtnvum a" <|
+            \() ->
+                let
+                    initialXaxahraparak =
+                        [ { occupants = [], x = 0 }, { occupants = [], x = 1 } ]
 
-      , test "ete qcac texy arden zbaxvac a, darnum en erku hogov" <|
-        \() ->
-          let
-            prepopulatedXaxahraparak = [{occupants = [], x = 0}, {occupants = [Erexa], x = 1}]
-            repopulatedXaxahraparak = populate prepopulatedXaxahraparak 1 Erexa
-            gaugeXaxahraparak = [{occupants = [], x = 0}, {occupants = [Erexa, Erexa], x = 1}]
-          in
-            Expect.equal repopulatedXaxahraparak gaugeXaxahraparak
+                    populatedXaxahraparak =
+                        populate initialXaxahraparak 1 Erexa
 
-    ]
+                    gaugeXaxahraparak =
+                        [ { occupants = [], x = 0 }, { occupants = [ Erexa ], x = 1 } ]
+                in
+                Expect.equal populatedXaxahraparak gaugeXaxahraparak
+        , test "ete qcac texy arden zbaxvac a, darnum en erku hogov" <|
+            \() ->
+                let
+                    prepopulatedXaxahraparak =
+                        [ { occupants = [], x = 0 }, { occupants = [ Erexa ], x = 1 } ]
 
+                    repopulatedXaxahraparak =
+                        populate prepopulatedXaxahraparak 1 Erexa
+
+                    gaugeXaxahraparak =
+                        [ { occupants = [], x = 0 }, { occupants = [ Erexa, Erexa ], x = 1 } ]
+                in
+                Expect.equal repopulatedXaxahraparak gaugeXaxahraparak
+        ]
